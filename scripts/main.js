e6a1178e49e6949c3b66a80a8efb8aad26ac4863
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-03-06 09:41:57 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-window.onload = function() {
+window.onload = setTimeout(function() {
   const spinner = document.getElementById('loading');
  
   // Add .loaded to .loading
   spinner.classList.add('loaded');
-}
+}, 5000);
